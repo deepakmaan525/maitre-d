a0fa584f6c97300f2d43493f00b571647722c78b
deepakmaan525 <deepakmaan525@gmail.com>
2023-12-02 15:37:36 +0530

    modified footer and added meta tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import footerimg from '../assets/images/footer-logo.svg'
 
 const Footer = () => {
     return (
-        <div className='footer-bg position-relative'>
+        <div className='footer-bg relative'>
             <div className="container pt-50">
                 <div className='d-flex flex-column items-center justify-center pt-md-60'>
                     <div className="text-center"><img className='pb-15 footer-logo' src={footerimg} alt="fooeter-img" /></div>
@@ -14,10 +14,13 @@ const Footer = () => {
                     <p className='fs-sm ff-work text-semibold  text-white'>Navigation</p>
                     <p className='fs-sm ff-work text-semibold  text-white'>Company</p>
                     <p className='fs-sm ff-work text-semibold  text-white'>Contact</p>
-
                 </div>
             </div>
             <p className='ff-work text-normal fs-s-sm text-gray text-center footer-line py-13'>Copyright 2020 All rights reserved, Greenfox Development Pty Ltd©</p>
+            <div className="footer-ellipse1"></div>
+            <div className="footer-ellipse2"></div>
+            <div className="footer-ellipse3"></div>
+            <div className="footer-ellipse4"></div>
         </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import curluImg from '../assets/images/curly-img.png'
 const Hero = () => {
   return (
     <>
-      <div className="container">
+      <div className="container relative">
         <h1 className='fs-xxl text-bolder text-black pt-md-90 text-center max-w-755 mx-auto ff-work'>Maitre D – Store Management for Barbers</h1>
         <div className="text-center pt-60"><button className='primary-btn text-white fs-md ff-work text-semibold bg-orange-gradient'>Submit</button></div>
+        <img className='hero-ellpse' src={curluImg} alt="ellipse" />
       </div>
     </>
   )
